9289ab4dd73a9aa341ec070eb7f547913d38a07e
Erik de Castro Lopo <erikd@mega-nerd.com>
2026-06-13 15:55:06 +1000

    Initial commit
    Take the slightly modified benchmark from git HEAD of
    
       https://github.com/nikita-volkov/hasql
    
    and modify it to be buildable with hasql ^>= 1.9 and hasql ^>= 1.10.

---
+++ src/benchmarks/Main.hs
@@ -0,0 +1,146 @@
+{-# LANGUAGE CPP #-}
+module Main where
+
+import Criterion
+import Criterion.Main
+import Hasql.Connection qualified as A
+import Hasql.Decoders qualified as D
+import Hasql.Pipeline qualified as E
+import Hasql.Session qualified as B
+import Hasql.Statement qualified as C
+import Prelude
+
+main :: IO ()
+main =
+  do
+    Right connection <- acquireConnection
+    useConnection connection
+  where
+    acquireConnection =
+      A.acquire mempty
+    useConnection connection =
+      defaultMain
+        [ sessionBench "largeResultInVector" sessionWithSingleLargeResultInVector,
+          sessionBench "largeResultInList" sessionWithSingleLargeResultInList,
+          sessionBench "manyLargeResults" sessionWithManyLargeResults,
+          sessionBench "manyLargeResultsViaPipeline" sessionWithManyLargeResultsViaPipeline,
+          sessionBench "manySmallResults" sessionWithManySmallResults,
+          sessionBench "manySmallResultsViaPipeline" sessionWithManySmallResultsViaPipeline,
+          -- Benchmarks targeting the per-statement pipeline overhead introduced in 1.10.
+          --
+          -- Session.statement is now routed through pipeline mode even for individual
+          -- queries, adding enterPipelineMode + pipelineSync (an extra server round-trip)
+          -- + exitPipelineMode + mask per statement.  The groups below make that cost
+          -- visible: compare the per-statement time of "sequential" (N × Session.statement)
+          -- against "pipeline" (Session.pipeline with N statements).  The difference is
+          -- the per-call overhead imposed by the pipeline wrapper on sequential execution.
+          bgroup
+            "singleStatementOverhead"
+            [ sessionBench "1-sequential" sessionWith1SmallResult,
+              sessionBench "1-pipeline" sessionWith1SmallResultViaPipeline,
+              sessionBench "10-sequential" sessionWith10SmallResults,
+              sessionBench "10-pipeline" sessionWith10SmallResultsViaPipeline,
+              sessionBench "100-sequential" sessionWithManySmallResults,
+              sessionBench "100-pipeline" sessionWithManySmallResultsViaPipeline
+            ]
+        ]
+      where
+        sessionBench :: (NFData a) => String -> B.Session a -> Benchmark
+        sessionBench name session =
+#if MIN_VERSION_hasql (1,10,0)
+          bench name (nfIO (A.use connection session >>= either (fail . show) pure))
+#else
+          bench name (nfIO (B.run session connection >>= either (fail . show) pure))
+#endif
+
+-- * Sessions
+
+sessionWith1SmallResult :: B.Session (Int64, Int64)
+sessionWith1SmallResult =
+  B.statement () statementWithSingleRow
+
+sessionWith1SmallResultViaPipeline :: B.Session (Int64, Int64)
+sessionWith1SmallResultViaPipeline =
+  B.pipeline (E.statement () statementWithSingleRow)
+
+sessionWith10SmallResults :: B.Session [(Int64, Int64)]
+sessionWith10SmallResults =
+  replicateM 10 (B.statement () statementWithSingleRow)
+
+sessionWith10SmallResultsViaPipeline :: B.Session [(Int64, Int64)]
+sessionWith10SmallResultsViaPipeline =
+  B.pipeline (replicateM 10 (E.statement () statementWithSingleRow))
+
+sessionWithSingleLargeResultInVector :: B.Session (Vector (Int64, Int64))
+sessionWithSingleLargeResultInVector =
+  B.statement () statementWithManyRowsInVector
+
+sessionWithSingleLargeResultInList :: B.Session [(Int64, Int64)]
+sessionWithSingleLargeResultInList =
+  B.statement () statementWithManyRowsInList
+
+sessionWithManyLargeResults :: B.Session [Vector (Int64, Int64)]
+sessionWithManyLargeResults =
+  replicateM 100 (B.statement () statementWithManyRowsInVector)
+
+sessionWithManySmallResults :: B.Session [(Int64, Int64)]
+sessionWithManySmallResults =
+  replicateM 100 (B.statement () statementWithSingleRow)
+
+sessionWithManyLargeResultsViaPipeline :: B.Session [Vector (Int64, Int64)]
+sessionWithManyLargeResultsViaPipeline =
+  B.pipeline (replicateM 100 (E.statement () statementWithManyRowsInVector))
+
+sessionWithManySmallResultsViaPipeline :: B.Session [(Int64, Int64)]
+sessionWithManySmallResultsViaPipeline =
+  B.pipeline (replicateM 100 (E.statement () statementWithSingleRow))
+
+-- * Statements
+
+statementWithSingleRow :: C.Statement () (Int64, Int64)
+statementWithSingleRow =
+#if MIN_VERSION_hasql (1,10,0)
+  C.preparable template encoder decoder
+#else
+  C.Statement template encoder decoder True
+#endif     
+  where
+    template =
+      "SELECT 1::int8, 2::int8"
+    encoder =
+      conquer
+    decoder =
+      D.singleRow row
+      where
+        row =
+          tuple <$> (D.column . D.nonNullable) D.int8 <*> (D.column . D.nonNullable) D.int8
+          where
+            tuple !a !b =
+              (a, b)
+
+statementWithManyRows :: (D.Row (Int64, Int64) -> D.Result result) -> C.Statement () result
+statementWithManyRows decoder =
+#if MIN_VERSION_hasql (1,10,0)
+  C.preparable template encoder (decoder rowDecoder)
+#else
+  C.Statement template encoder (decoder rowDecoder) True
+#endif
+    
+  where
+    template =
+      "SELECT generate_series(0,1000)::int8 as a, generate_series(1000,2000)::int8 as b"
+    encoder =
+      conquer
+    rowDecoder =
+      tuple <$> (D.column . D.nonNullable) D.int8 <*> (D.column . D.nonNullable) D.int8
+      where
+        tuple !a !b =
+          (a, b)
+
+statementWithManyRowsInVector :: C.Statement () (Vector (Int64, Int64))
+statementWithManyRowsInVector =
+  statementWithManyRows D.rowVector
+
+statementWithManyRowsInList :: C.Statement () [(Int64, Int64)]
+statementWithManyRowsInList =
+  statementWithManyRows D.rowList
